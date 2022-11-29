8dde40c8e677b037d1844e1e3f1e983ab4f35ef6
arsenkhoIod <arsenykholod@yandex.ru>
2022-11-30 01:22:37 +0300

    function completed

--- TrafficLight2/ViewController.swift
+++ TrafficLight2/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    
+    @IBAction func switchingColor() {
+        switchButton.setTitle("NEXT", for: .normal)
+        if redLight.alpha != 1 && yellowLight.alpha != 1 && greenLight.alpha != 1 {
+            redLight.alpha = 1
+        } else {
+            if redLight.alpha == 1 {
+                redLight.alpha = 0.3
+                yellowLight.alpha = 1
+            } else if yellowLight.alpha == 1 {
+                yellowLight.alpha = 0.3
+                greenLight.alpha = 1
+            } else {
+                greenLight.alpha = 0.3
+                redLight.alpha = 1
+            }
+        }
+        
+    }
+    
+    
 }
 
